71e0fc7ff3382deaf266d82c3ddf9a4560042269
Henrico Pops <hpops@bloemert.com>
2019-11-20 17:04:25 +0100

    #58 - Added a summary for the ReturnType check

--- IDesign/IDesign.Check/MethodChecks.cs
+++ IDesign/IDesign.Check/MethodChecks.cs
@@ -4,6 +4,12 @@ namespace IDesign.Checks
 {
     public static class MethodChecks
     {
+        /// <summary>
+        /// Return a boolean based on if the given method returns the expected type
+        /// </summary>
+        /// <param name="methodSyntax">The method witch it should check</param>
+        /// <param name="returnType">The expected return type</param>
+        /// <returns></returns>
         public static bool CheckReturnType(this MethodDeclarationSyntax methodSyntax, string returnType)
         {
             return methodSyntax.ReturnType.ToString() == returnType;
