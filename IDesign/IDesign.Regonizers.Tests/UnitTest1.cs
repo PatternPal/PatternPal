9f9e8efb073fdcc6c130ef491fa84d79eecd0269
Henrico Pops <hpops@bloemert.com>
2019-11-20 11:54:44 +0100

    NUnit as test framework

--- IDesign/IDesign.Regonizers.Tests/UnitTest1.cs
+++ IDesign/IDesign.Regonizers.Tests/UnitTest1.cs
@@ -1,16 +1,18 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 
 namespace IDesign.Regonizers.Tests
 {
-    [TestClass]
-    public class UnitTest1
+    public class Tests
     {
-        [TestMethod]
-        public void TestMethod1()
+        [SetUp]
+        public void Setup()
         {
-            var s = new SingletonRegonizer();
-            Assert.IsNull(s.Regonize());
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
         }
     }
 }
