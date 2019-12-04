1db593d0371694aa16cb67af1fc4130bf2930bbd
Henrico Pops <hpops@bloemert.com>
2019-12-04 16:15:09 +0100

    #80 - Adapter pattern recognizer

--- IDesign/IDesign.Regonizers/AdapterRecognizer.cs
+++ IDesign/IDesign.Regonizers/AdapterRecognizer.cs
@@ -4,6 +4,7 @@ using IDesign.Models;
 using IDesign.Recognizers.Abstractions;
 using IDesign.Recognizers.Output;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace IDesign.Recognizers
 {
@@ -12,30 +13,24 @@ namespace IDesign.Recognizers
         public IResult Recognize(IEntityNode entityNode)
         {
             var result = new Result();
-            var methodChecks = new List<ElementCheck<IMethod>>()
-            {
-                new ElementCheck<IMethod>(x => x.CheckReturnType(entityNode.GetName()) , "Incorrecte return type"),
-                new ElementCheck<IMethod>(x => x.CheckModifier("static") , "Is niet static"),
-                new ElementCheck<IMethod>(x => x.CheckReturnTypeSameAsCreation(), "Return type is niet hetzelfde als wat er gemaakt wordt" )
-            };
-            CheckElements(result, entityNode.GetMethods(), methodChecks);
+            // Adapter
+            // Implements interface
+            if (entityNode.GetRelations().Any(x => x.GetRelationType() == RelationType.Implements))
+                result.Score += 1;
 
-            var propertyChecks = new List<ElementCheck<IField>>()
-            {
-               new ElementCheck<IField>(x => x.CheckFieldType(entityNode.GetName()) , "Incorrecte type"),
-               new ElementCheck<IField>(x => x.CheckMemberModifier("static") , "Is niet static"),
-               new ElementCheck<IField>(x => x.CheckMemberModifier("private") , "Is niet private")
-            };
-            CheckElements(result, entityNode.GetFields(), propertyChecks);
+            // Has field with class (adapter)
+            // As many public functions as adapter
+            // All functions uses the adapter
 
-            var constructorChecks = new List<ElementCheck<IMethod>>()
-            {
-                 new ElementCheck<IMethod>(x => !x.CheckModifier("public") , "Is public moet private of protected zijn")
-            };
-            CheckElements(result, entityNode.GetConstructors(), constructorChecks);
+            //result.Score = (int)(result.Score / 7f * 100f);
+            entityNode.GetTy
 
-            result.Score = (int)(result.Score / 7f * 100f);
+
             return result;
         }
+
+        
+
+
     }
 }
