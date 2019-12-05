34444083dc20a033539408042a6a41086e9c7298
MMCQ33 <jwat555@hotmail.com>
2019-12-05 16:08:53 +0100

    FileUtils summeries
    Made Summeries for the fileUtils class

--- IDesign/IDesign.Tests/Utils/FileUtils.cs
+++ IDesign/IDesign.Tests/Utils/FileUtils.cs
@@ -7,6 +7,11 @@ namespace IDesign.Tests.Utils
     public static class FileUtils
     {
 
+        /// <summary>
+        /// Returns a string of all the contenst from the given file. 
+        /// </summary>
+        /// <param name="fileName">Path of the file which needs to be read</param>
+        /// <returns>The file content</returns>
         public static string FileToString(string fileName)
         {
             try
@@ -20,6 +25,11 @@ namespace IDesign.Tests.Utils
             }
         }
 
+        /// <summary>
+        /// returns a list of strings of the contents of all the files
+        /// </summary>
+        /// <param name="folderPath">Path of the folder in which all files need to be read</param>
+        /// <returns>List with all file contents </returns>
         public static List<string> FilesToString(string folderPath)
         {
             var filesContents = new List<string>();
