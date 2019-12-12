3ad66de9e34b6be474cc9437ca64ff711308492e
@s1113378 <s1113378@student.windesheim.nl>
2019-12-12 23:43:13 +0100

    merge

--- IDesign/IDesign.Tests/Recognizers/DecoratorRecognizerTest.cs
+++ IDesign/IDesign.Tests/Recognizers/DecoratorRecognizerTest.cs
@@ -15,6 +15,8 @@ namespace IDesign.Tests.Recognizers
         [TestCase("DecoratorTestCase1", 100)]
         [TestCase("DecoratorTestCase2", 0)]
         [TestCase("DecoratorTestCase3", 13)]
+        [TestCase("DecoratorTestCase4", 40)]
+        [TestCase("DecoratorTestCase5", 53)]
         public void DecoratorRecognizer_Returns_Correct_Score(string directoryPath, int score)
         {
             FileManager manager = new FileManager();
@@ -27,42 +29,5 @@ namespace IDesign.Tests.Recognizers
 
             Assert.AreEqual(score, result.Last().Result.GetScore());
         }
-
-        /*[TestCase("DecoratorTestCase1.cs", 100)]
-        [TestCase("DecoratorTestCase2.cs", 100)]
-        [TestCase("DecoratorTestCase3.cs", 100)]
-        [TestCase("DecoratorTestCase4.cs", 50)]
-        [TestCase("DecoratorTestCase5.cs", 50)]
-        [TestCase("DecoratorTestCase6.cs", 50)]
-        [TestCase("DecoratorTestCase7.cs", 0)]
-        public void DecoratorRecognizer_Returns_Correct_Score(string filename, int score)
-        {
-            var decorator = new DecoratorRecognizer();
-            string code = FileUtils.FileToString("Decorator\\" + filename);
-
-
-            var root = CSharpSyntaxTree.ParseText(code).GetCompilationUnitRoot();
-            var NameSpaceNode = root.Members[0] as NamespaceDeclarationSyntax;
-            var testNode = NameSpaceNode.Members[0] as ClassDeclarationSyntax;
-
-            var entityNode = new EntityNode
-            {
-                Name = testNode.Identifier.ToString(),
-                InterfaceOrClassNode = testNode,
-
-                MethodDeclarationSyntaxList =
-                testNode.DescendantNodes().OfType<MethodDeclarationSyntax>().ToList(),
-                FieldDeclarationSyntaxList =
-                testNode.DescendantNodes().OfType<FieldDeclarationSyntax>().ToList(),
-                PropertyDeclarationSyntaxList =
-                testNode.DescendantNodes().OfType<PropertyDeclarationSyntax>().ToList(),
-                ConstructorDeclarationSyntaxList =
-                testNode.DescendantNodes().OfType<ConstructorDeclarationSyntax>().ToList()
-            };
-
-            var result = decorator.Recognize(entityNode);
-
-            Assert.AreEqual(score, result.GetScore());
-        }*/
     }
 }
