bb0e04c2b15005b87072c9b4b274750971a43339
Henrico Pops <hpops@bloemert.com>
2019-12-13 10:40:16 +0100

    Adapter pattern

--- IDesign/IDesign.Regonizers/AdapterRecognizer.cs
+++ IDesign/IDesign.Regonizers/AdapterRecognizer.cs
@@ -1,6 +1,6 @@
-﻿using IDesign.Recognizers.Abstractions;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
+using IDesign.Recognizers.Abstractions;
 using IDesign.Recognizers.Checks;
 using IDesign.Recognizers.Models;
 using IDesign.Recognizers.Models.ElementChecks;
@@ -12,50 +12,47 @@ namespace IDesign.Recognizers
     {
         public IResult Recognize(IEntityNode entityNode)
         {
-            // Adapter
-            // Implements interface
-
-
             var result = new Result();
             IRelation currentRelation = null;
             string currentField = null;
-            var singletonCheck = new GroupCheck<IEntityNode, IEntityNode>(new List<ICheck<IEntityNode>>
+            var adapterCheck = new GroupCheck<IEntityNode, IEntityNode>(new List<ICheck<IEntityNode>>
             {
-                new ElementCheck<IEntityNode>(x => x.GetRelations().Any(y=> y.GetRelationType() == RelationType.Implements), "Implements interface"),
+                new ElementCheck<IEntityNode>(
+                    x => x.GetRelations().Any(y => y.GetRelationType() == RelationType.Implements || y.GetRelationType() == RelationType.Extends),
+                    "Implements interface or extends class"),
                 new GroupCheck<IEntityNode, IRelation>(new List<ICheck<IRelation>>
                 {
+                    //Is used by adapter    
                     new ElementCheck<IRelation>(x =>
                     {
                         currentRelation = x;
                         return x.GetRelationType() == RelationType.Uses;
                     }, "Is used by adapter"),
+
+                    //Adapter has an adaptee field
                     new GroupCheck<IRelation, IField>(new List<ICheck<IField>>
                     {
-                       new ElementCheck<IField>(x =>
-                       {
-                           currentField = x.GetName();
-                           return x.CheckFieldType(currentRelation.GetDestination().GetName());
-                       }, "Field has adaptee as type"),
-                       new GroupCheck<IField, IMethod>(new List<ICheck<IMethod>>
-                       {
-                           new ElementCheck<IMethod>(x => x.CheckFieldIsUsed(currentField), "Method uses adpatee")
-                       }, x => entityNode.GetMethods(), "Is used in every adapter method", GroupCheckType.All)
+                        new ElementCheck<IField>(x =>
+                        {
+                            currentField = x.GetName();
+                            return x.CheckFieldType(currentRelation.GetDestination().GetName());
+                        }, "Field has adaptee as type"),
+                        new GroupCheck<IField, IMethod>(new List<ICheck<IMethod>>
+                        {
+                            new ElementCheck<IMethod>(x => x.CheckFieldIsUsed(currentField), "Method uses adpatee"),
+                            new ElementCheck<IMethod>(x => !x.CheckReturnType(currentField), "Method does not return adaptee"),
+
+                        }, x => entityNode.GetMethods(), "Is used in every adapter method", GroupCheckType.All)
+
                     }, x => entityNode.GetFields(), "Adapter has an adaptee field")
                 }, node => node.GetRelations(), "Has adaptee")
             }, x => new List<IEntityNode> { entityNode }, "Adapter", GroupCheckType.All);
 
 
-            var r = singletonCheck.Check(entityNode);
+            var r = adapterCheck.Check(entityNode);
 
             result.Results = r.GetChildFeedback().ToList();
             return result;
-
-
-            return result;
         }
-
-
-
-
     }
 }
---
+++ IDesign/IDesign.Tests/Recognizers/AdapterRecognizer.cs
@@ -0,0 +1,33 @@
+﻿using IDesign.Core;
+using IDesign.Recognizers;
+using IDesign.Tests.TestClasses.StateTest3;
+using IDesign.Tests.Utils;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IDesign.Tests.Recognizers
+{
+    class AdapterRecognizerTest
+    {
+        [TestCase("AdapterTest1", "Target", 0, 40)]
+        [TestCase("AdapterTest1", "Adaptee", 0, 40)]
+        [TestCase("AdapterTest1", "Adapter", 90, 100)]
+        public void AdapterRecognizer_Returns_Correct_Score(string directory, string filename, int minscore, int maxscore)
+        {
+            var state = new AdapterRecognizer();
+            var filesAsString = FileUtils.FilesToString($"{directory}\\");
+            var nameSpaceName = $"IDesign.Tests.TestClasses.{directory}";
+            var entityNodes = EntityNodeUtils.CreateEntityNodeGraph(filesAsString);
+            var createRelation = new DetermineRelations(entityNodes);
+            createRelation.GetEdgesOfEntityNode();
+            var result = state.Recognize(entityNodes[nameSpaceName + "." + filename]);
+
+            Assert.That(result.GetScore(), Is.InRange(minscore, maxscore));
+        }
+    }
+}
