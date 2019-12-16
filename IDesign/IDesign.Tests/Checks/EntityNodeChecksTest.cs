8e4b3293bbf5c57ecb5614dd7c0b6365f8ae1b34
Henrico Pops <hpops@bloemert.com>
2019-12-16 22:09:24 +0100

    Fix build errors

--- IDesign/IDesign.Tests/Checks/EntityNodeChecksTest.cs
+++ IDesign/IDesign.Tests/Checks/EntityNodeChecksTest.cs
@@ -105,7 +105,7 @@ namespace IDesign.Tests.Checks
             var nodes = EntityNodeUtils.CreateEntityNodeGraph(code);
             var createRelation = new DetermineRelations(nodes);
             createRelation.GetEdgesOfEntityNode();
-            var checkResult = nodes[nameSpaceName + "." + className].ClassImlementsInterface(interfaceName);
+            var checkResult = nodes[nameSpaceName + "." + className].ClassImplementsInterface(interfaceName);
 
             Assert.AreEqual(shouldBeValid, checkResult);
         }
