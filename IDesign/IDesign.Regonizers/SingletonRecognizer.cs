e3f94d945f8c86799a18c78e1e65a8a67b4db872
Shanna99 <shannavangrevengoed@live.nl>
2020-01-07 16:14:13 +0100

    punten

--- IDesign/IDesign.Regonizers/SingletonRecognizer.cs
+++ IDesign/IDesign.Regonizers/SingletonRecognizer.cs
@@ -15,22 +15,22 @@ namespace IDesign.Recognizers
 
             var methodChecks = new List<ICheck<IMethod>>
             {
-                new ElementCheck<IMethod>(x => x.CheckReturnType(entityNode.GetName()), "Incorrect return type"),
-                new ElementCheck<IMethod>(x => x.CheckModifier("static"), "Is not static"),
+                new ElementCheck<IMethod>(x => x.CheckReturnType(entityNode.GetName()), "Incorrect return type",1),
+                new ElementCheck<IMethod>(x => x.CheckModifier("static"), "Is not static",1),
                 new ElementCheck<IMethod>(x => x.CheckReturnTypeSameAsCreation(),
                     "Return type isnt the same as created")
             };
 
             var propertyChecks = new List<ICheck<IField>>
             {
-                new ElementCheck<IField>(x => x.CheckFieldType(new List<string>(){ entityNode.GetName() }), "Incorrect type"),
-                new ElementCheck<IField>(x => x.CheckMemberModifier("static"), "Is not static"),
-                new ElementCheck<IField>(x => x.CheckMemberModifier("private"), "Is not private")
+                new ElementCheck<IField>(x => x.CheckFieldType(new List<string>(){ entityNode.GetName() }), "Incorrect type",1),
+                new ElementCheck<IField>(x => x.CheckMemberModifier("static"), "Is not static",1),
+                new ElementCheck<IField>(x => x.CheckMemberModifier("private"), "Is not private",1)
             };
 
             var constructorChecks = new List<ICheck<IMethod>>
             {
-                new ElementCheck<IMethod>(x => !x.CheckModifier("public"), "Is public must be private or protected")
+                new ElementCheck<IMethod>(x => (x.CheckModifier("private")) ||(x.CheckModifier("protected")), "should be private or protected",1)
             };
 
 
@@ -39,7 +39,7 @@ namespace IDesign.Recognizers
                 new GroupCheck<IEntityNode, IMethod>(methodChecks, x => x.GetMethods(), "Has GetInstance()"),
                 new GroupCheck<IEntityNode, IField>(propertyChecks, x => x.GetFields(), "Has instance of itself"),
                 new GroupCheck<IEntityNode, IMethod>(constructorChecks, x => x.GetConstructors(), "Private constructor")
-            }, x => new List<IEntityNode> {entityNode}, "Singleton", GroupCheckType.All);
+            }, x => new List<IEntityNode> { entityNode }, "Singleton", GroupCheckType.All);
 
 
             var r = singletonCheck.Check(entityNode);
