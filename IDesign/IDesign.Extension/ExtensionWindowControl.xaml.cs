54d2f037441a22f3eab859c14c7359769d42ec22
Nick Chen <NXDChen@github.com>
2020-01-09 00:35:21 +0100

    save current file

--- IDesign/IDesign.Extension/ExtensionWindowControl.xaml.cs
+++ IDesign/IDesign.Extension/ExtensionWindowControl.xaml.cs
@@ -124,22 +124,7 @@ namespace IDesign.Extension
 
         private void SaveAllFiles()
         {
-            var soln = Dte.Solution;
-
-            for (int i = 1; i <= soln.Projects.Count; i++)
-            {
-                if (!soln.Projects.Item(i).Saved)
-                {
-                    soln.Projects.Item(i).Save();
-                }
-                for (int j = 1; j <= soln.Projects.Item(i).ProjectItems.Count; j++)
-                {
-                    if (!soln.Projects.Item(i).ProjectItems.Item(j).Saved)
-                    {
-                        soln.Projects.Item(i).ProjectItems.Item(j).Save();
-                    }
-                }
-            }
+            Dte.ActiveDocument.Save();
         }
 
         /// <summary>
