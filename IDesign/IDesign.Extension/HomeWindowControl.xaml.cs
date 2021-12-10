31068d97ddfffde9e413f99195ee1b336f953a28
Frank <frankstam01@gmail.com>
2021-12-10 10:26:46 +0100

    Home screen is done

--- IDesign/IDesign.Extension/HomeWindowControl.xaml.cs
+++ IDesign/IDesign.Extension/HomeWindowControl.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace IDesign.Extension
 {
@@ -27,7 +15,7 @@ namespace IDesign.Extension
 
         private void HomeWindowControl_OnSizeChanged(object sender, SizeChangedEventArgs e)
         {
-            ButtonsPanel.Orientation = e.NewSize.Height >= e.NewSize.Width ? Orientation.Vertical : Orientation.Horizontal;
+            ButtonsPanel.Orientation = e.NewSize.Width >= 570 ? Orientation.Horizontal : Orientation.Vertical;
         }
     }
 }
