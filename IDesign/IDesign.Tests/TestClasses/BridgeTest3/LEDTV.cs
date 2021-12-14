06159c9cdf40200833767bfcad7e9e7b51b20439
Zeewar Ghafir <ghafirzeewar@gmail.com>
2021-12-14 09:58:45 +0100

    Added test classes for the bridge pattern

---
+++ IDesign/IDesign.Tests/TestClasses/BridgeTest3/LEDTV.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IDesign.Tests.TestClasses.BridgeTest3
+{
+    public interface LEDTV
+    {
+        void SwitchOn();
+        void SwitchOff();
+        void SetChannel(int channelNumber);
+    }
+}
---
+++ IDesign/IDesign.Tests/TestClasses/BridgeTest3/SonyRemoteControl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IDesign.Tests.TestClasses.BridgeTest3
+{
+    public class SonyRemoteControl : AbstractRemoteControl
+    {
+        public SonyRemoteControl(LEDTV ledTv) : base(ledTv)
+        {
+        }
+
+        public override void SwitchOn()
+        {
+            ledTv.SwitchOn();
+        }
+
+        public override void SwitchOff()
+        {
+            ledTv.SwitchOff();
+        }
+
+        public override void SetChannel(int channelNumber)
+        {
+            ledTv.SetChannel(channelNumber);
+        }
+    }
+}
