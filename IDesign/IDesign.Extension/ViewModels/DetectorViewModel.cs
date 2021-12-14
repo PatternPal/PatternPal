77b27125db11f2a8e8c0eb2206b37be3fdc829d4
Frank <frankstam01@gmail.com>
2021-12-14 12:07:58 +0100

    Added new strings to ExtensionUIResources.resx Replaced button and description text in homescreen with strings from ExtensionUIResources.resx

--- IDesign/IDesign.Extension/ViewModels/DetectorViewModel.cs
+++ IDesign/IDesign.Extension/ViewModels/DetectorViewModel.cs
@@ -8,6 +8,6 @@ namespace IDesign.Extension.ViewModels
 {
     public class DetectorViewModel : ViewModel
     {
-        public override string Title => Resources.ExtensionUIResources.DetectorViewTitle;
+        public override string Title => Resources.ExtensionUIResources.DetectorTitle;
     }
 }
