d948c3acd3cd83f4e14eed2ff74ae8dde75ac863
Frank <frankstam01@gmail.com>
2021-12-22 12:01:21 +0100

    Pattern select screen (StepByStepList) is finished. - Created a new Back-end project, 'IDesign.StepByStep'. This project contains an interface for pattern instruction sets (IInstructionSet) which is currently implemented by StrategyInstructionSet.cs - The step by step list view loads the patterns from a list in the back-end (InstructionSetsCreator)

--- IDesign/IDesign.Extension/ViewModels/StepByStepListViewModel.cs
+++ IDesign/IDesign.Extension/ViewModels/StepByStepListViewModel.cs
@@ -1,6 +1,12 @@
-﻿using System.Windows.Input;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
 using IDesign.Extension.Commands;
 using IDesign.Extension.Stores;
+using IDesign.StepByStep;
+using IDesign.StepByStep.Abstractions;
+
 
 namespace IDesign.Extension.ViewModels
 {
@@ -10,13 +16,15 @@ namespace IDesign.Extension.ViewModels
 
         public ICommand NavigateHomeCommand { get; }
         public ICommand NavigateStepByStepInstructionsCommand { get; }
-
+        public List<IInstructionSet> InstructionSetList { get; set; }
+        public IInstructionSet FirstInstructionSet { get; set; }
 
         public StepByStepListViewModel(NavigationStore navigationStore)
         {
             NavigateHomeCommand = new NavigateCommand<HomeViewModel>(navigationStore, () => new HomeViewModel(navigationStore));
             NavigateStepByStepInstructionsCommand = new NavigateCommand<StepByStepInstructionsViewModel>(navigationStore, () => new StepByStepInstructionsViewModel(navigationStore));
-
+            InstructionSetList = InstructionSetsCreator.InstructionSets;
+            FirstInstructionSet = InstructionSetList?.FirstOrDefault();
         }
     }
 }
---
+++ IDesign/IDesign.StepByStep/InstructionSets/StrategyInstructionSet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IDesign.StepByStep.Abstractions;
+using IDesign.Core.Resources;
+using static IDesign.Core.Resources.DesignPatternNameResources;
+
+namespace IDesign.StepByStep.InstructionSets
+{
+    internal class StrategyInstructionSet : IInstructionSet
+    {
+        public string InstructionName => Strategy;
+    }
+}
