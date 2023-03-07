54033add48539ef7cc99a73f070066c74c84a87c
Casper Verhaar <casper.verhaar@gmail.com>
2023-03-07 17:32:53 +0100

    feat: Add background service project

---
+++ PatternPal/PatternPal/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PatternPal;
+
+internal static class Program
+{
+    internal static void Main()
+    {
+        var builder = WebApplication.CreateBuilder();
+
+        // Additional configuration is required to successfully run gRPC on macOS.
+        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+        // Add services to the container.
+        builder.Services.AddGrpc();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        app.MapGrpcService< GreeterService >();
+        app.MapGet(
+            "/",
+            () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+        app.Run();
+    }
+}
