b383272c06fe309d484022c2a5d30aa8b58c2ef9
Siem van den Tweel <siemvdtweel@gmail.com>
2023-03-23 13:16:46 +0100

    LogServer setup

---
+++ PatternPal/PatternPal.LoggingServer/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using PatternPal.LoggingServer.Services;
+
+namespace PatternPal;
+
+internal static class Program
+{
+    internal static void Main()
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder();
+
+        // Additional configuration is required to successfully run gRPC on macOS.
+        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+        builder.Services.AddGrpc();
+
+        WebApplication app = builder.Build();
+
+        //app.UseGrpcWeb(
+        //    new GrpcWebOptions
+        //    {
+        //        DefaultEnabled = true
+        //    });
+
+        app.MapGrpcService<LoggerService>();
+
+        app.Run();
+    }
+}
---
+++ PatternPal/PatternPal.LoggingServer/Services/LoggerService.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Core;
+using PatternPal.LoggingServer;
+
+namespace PatternPal.LoggingServer.Services
+{
+    public class LoggerService : Log.LogBase
+    {
+        private readonly ILogger<LoggerService> _logger;
+        public LoggerService(ILogger<LoggerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<LogReply> Log(LogRequest request, ServerCallContext context)
+        {
+            
+            // TODO EVERYTHING
+
+            return Task.FromResult(new LogReply
+            {
+                Message = "Logged"
+            });
+        }
+    }
+}
