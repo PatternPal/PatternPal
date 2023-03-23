3f8cb0d92a84f9a7fbe754cbcce5ad9c78cd8da4
Siem van den Tweel <siemvdtweel@gmail.com>
2023-03-23 14:12:46 +0100

    Log: Untested version of CSV manager and simple ProgSnap Class

---
+++ PatternPal/PatternPal.LoggingServer/Helpers/CsvManager.cs
@@ -0,0 +1,68 @@
+﻿namespace PatternPal.Helpers;
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Google.Protobuf.WellKnownTypes;
+using PatternPal.LoggingServer.Models;
+using Enum = System.Enum;
+
+public class CSVManager
+{
+    private CsvReader reader;
+    private CsvWriter writer;
+
+    public CSVManager(string filepath)
+    {
+        using StreamReader reader = new StreamReader(filepath);
+        using CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+
+        this.reader = csv;
+
+        var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            HasHeaderRecord = false,
+        };
+        //  TODO: Check if file exists, if not, create it and write header
+        StreamWriter writer = new StreamWriter(filepath, true);
+        CsvWriter csvWriter = new CsvWriter(writer, config);
+        this.writer = csvWriter;
+
+    }
+    public List<ProgSnap2Event> GetEvents(Dictionary<string, string > indexes)
+    {
+        List<ProgSnap2Event> events = new List<ProgSnap2Event>();
+        while (reader.Read())
+        {
+            ProgSnap2Event e;
+            foreach (KeyValuePair<string, string> index in indexes)
+            {
+                if (reader.GetField(index.Key) == index.Value)
+                {
+                    e = reader.GetRecord<ProgSnap2Event>();
+                    events.Add(e);
+                }
+            }   
+        }
+        return events;
+    }
+
+    public void WriteEvent(ProgSnap2Event e)
+    {
+        this.writer.WriteRecord(e);
+        this.writer.NextRecord();
+    }
+
+    public void WriteEvents(List<ProgSnap2Event> events)
+    {
+        foreach (ProgSnap2Event e in events)
+        {
+            this.writer.WriteRecord(e);
+            this.writer.NextRecord();
+        }
+    }
+
+
+
+
+}
---
+++ PatternPal/PatternPal.LoggingServer/Models/ProgSnap2.cs
@@ -0,0 +1,50 @@
+﻿using CsvHelper.Configuration.Attributes;
+using Google.Protobuf.WellKnownTypes;
+using PatternPal.LoggingServer.Models;
+
+namespace PatternPal.LoggingServer.Models
+{
+    public enum EventType
+    {
+        SessionStart,
+        SessionEnd,
+        ProjectOpen,
+        ProjectClose,
+        FileCreate,
+        FileDelete,
+        FileOpen,
+        FileClose,
+        FileFocus,
+        FileRename,
+        FileCopy,
+        FileEdit,
+        Compile,
+        CompileError,
+        CompileWarning,
+        Submit,
+        RunProgram,
+        DebugProgram,
+        RunTest,
+        DebugTest,
+        ResourceView,
+        Intervention,
+        Custom
+    }
+
+    public class ProgSnap2Event
+    {
+        [Name("EventID")]
+        public string EventID { get; set; }
+        [Name("SubjectID")]
+        public string SubjectID { get; set; }
+        public string ToolInstances { get; set; }
+        public string CodeStateID { get; set; }
+        public EventType EventType { get; set; }
+        public int? Order { get; set; }
+        public Timestamp? ServerTimeStamp { get; set; }
+        public TimeZoneInfo? ServerTimeZone { get; set; }
+        public Timestamp? ClientTimeStamp { get; set; }
+        public TimeZoneInfo? ClientTimeZone { get; set; }
+    }
+
+}
