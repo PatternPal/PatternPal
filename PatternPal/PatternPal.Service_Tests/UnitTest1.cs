211938031b5528b21e60c02c08a7d098fb71bcbf
DanielvanDamme <69575357+DanielvanDamme@users.noreply.github.com>
2023-03-27 16:19:17 +0200

    Initial commit

---
+++ PatternPal/PatternPal.Service_Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace PatternPal.Service_Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
