b33f62388c869fa3b667c2e1759715f345e4de16
Casper Verhaar <casper.verhaar@gmail.com>
2023-04-11 10:43:46 +0200

    Rec: Move CheckBuilder

--- PatternPal/PatternPal.Core/Builders/CheckBuilder.cs
+++ PatternPal/PatternPal.Core/Builders/CheckBuilder.cs
@@ -9,3 +9,32 @@ internal interface ICheckBuilder
 
     ICheck Build();
 }
+
+internal static class CheckBuilder
+{
+    internal static CheckCollectionBuilder Any(
+        params ICheckBuilder[ ] checkBuilders) => new(
+        CheckCollectionKind.Any,
+        checkBuilders );
+
+    internal static NotCheckBuilder Not(
+        ICheckBuilder checkBuilder) => new( checkBuilder );
+
+    internal static ClassCheckBuilder Class(
+        params ICheckBuilder[ ] checkBuilders) => new( checkBuilders );
+
+    internal static MethodCheckBuilder Method(
+        params ICheckBuilder[ ] checkBuilders) => new( checkBuilders );
+
+    internal static ModifierCheckBuilder Modifiers(
+        params IModifier[ ] modifiers) => new( modifiers );
+
+    internal static UsesCheckBuilder Uses(
+        Func< INode > getMatchedEntity) => new( getMatchedEntity );
+
+    internal static FieldCheckBuilder Field(
+        params ICheckBuilder[ ] checkBuilders) => new( checkBuilders );
+
+    internal static ConstructorCheckBuilder Constructor(
+        params ICheckBuilder[ ] checkBuilders) => new( checkBuilders );
+}
--- PatternPal/PatternPal.Core/Recognizers/SingletonRecognizer.cs
+++ PatternPal/PatternPal.Core/Recognizers/SingletonRecognizer.cs
@@ -1,4 +1,4 @@
-﻿using static PatternPal.Core.Recognizers.CheckBuilder;
+﻿using static PatternPal.Core.Builders.CheckBuilder;
 
 namespace PatternPal.Core.Recognizers;
 
@@ -43,33 +43,4 @@ internal class SingletonRecognizer
             )
         );
     }
-}
-
-internal static class CheckBuilder
-{
-    internal static CheckCollectionBuilder Any(
-        params ICheckBuilder[ ] checkBuilders) => new(
-        CheckCollectionKind.Any,
-        checkBuilders );
-
-    internal static NotCheckBuilder Not(
-        ICheckBuilder checkBuilder) => new( checkBuilder );
-
-    internal static ClassCheckBuilder Class(
-        params ICheckBuilder[ ] checkBuilders) => new( checkBuilders );
-
-    internal static MethodCheckBuilder Method(
-        params ICheckBuilder[ ] checkBuilders) => new( checkBuilders );
-
-    internal static ModifierCheckBuilder Modifiers(
-        params IModifier[ ] modifiers) => new( modifiers );
-
-    internal static UsesCheckBuilder Uses(
-        Func< INode > getMatchedEntity) => new( getMatchedEntity );
-
-    internal static FieldCheckBuilder Field(
-        params ICheckBuilder[ ] checkBuilders) => new( checkBuilders );
-
-    internal static ConstructorCheckBuilder Constructor(
-        params ICheckBuilder[ ] checkBuilders) => new( checkBuilders );
 }
