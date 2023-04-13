8e549fb9c78bdb2a492f0e5e569daeb2a5d257fa
Rutger <rutger.vincken@ziggo.nl>
2023-04-13 17:54:15 +0200

    Written something in fieldcheck, dont know if it can be used

--- PatternPal/PatternPal.Core/Checks/FieldCheck.cs
+++ PatternPal/PatternPal.Core/Checks/FieldCheck.cs
@@ -1,4 +1,7 @@
-﻿namespace PatternPal.Core.Checks;
+﻿using Microsoft.CodeAnalysis.VisualBasic;
+using PatternPal.Recognizers.Models.Checks;
+
+namespace PatternPal.Core.Checks;
 
 internal class FieldCheck : ICheck
 {
@@ -11,15 +14,57 @@ internal class FieldCheck : ICheck
 
     public ICheckResult Check(RecognizerContext ctx, INode node)
     {
-        if(node is not IField field) throw new NotImplementedException("Field Check was incorrect");
+        if (node is not IField field)
+        {
+            throw new NotImplementedException("Field check was incorrect");
+        }
 
         foreach (ICheck check in _checks) 
         {
-            if (!check.Check(ctx, node).Correctness) throw new NotImplementedException("Field Check was incorrect");
+            switch (check)
+            {
+                case ModifierCheck modifierCheck:
+                    {
+                        bool hasMatch = false;
+                        foreach (IModified modified in field.GetModifiers())
+                        {
+                            if (modifierCheck.Check
+                                (ctx,
+                                modified).Correctness)
+                            {
+                                hasMatch = true;
+                                break;
+                            }
+                        }
+                        if (!hasMatch)
+                        {
+                            throw new NotImplementedException("Modifier check was incorrect");
+                        }
+                        break;
+                    }
+                case TypeCheck typeCheck:
+                    {
+                        if (!check.Check(ctx, node).Correctness)
+                        {
+                            throw new NotImplementedException("Type check was incorrect");
+                        }
+                        break;
+                    }
+                case FieldCheck fieldCheck:
+                    {
+                        throw new NotImplementedException("Field check is not yet implemented");
+                    }
+                default:
+                    {
+                        Console.WriteLine($"Unexpected check: {check.GetType().Name}");
+                        break;
+                    }
+            }
+            //Kan weg denk ik
+            //if (!check.Check(ctx, node).Correctness) throw new NotImplementedException("Field Check was incorrect");
         }
 
         Console.WriteLine($"Got field {field}");
-
-        throw new NotImplementedException("Field Check was correct");
+        throw new NotImplementedException("Field check was correct");
     }
 }
