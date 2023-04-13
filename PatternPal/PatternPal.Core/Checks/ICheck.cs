20354e90c4636d99c98dd4a261da951bbcad5457
Casper Verhaar <casper.verhaar@gmail.com>
2023-04-13 15:59:58 +0200

    Core: Add helper methods for writing checks

--- PatternPal/PatternPal.Core/Checks/ICheck.cs
+++ PatternPal/PatternPal.Core/Checks/ICheck.cs
@@ -14,3 +14,42 @@ enum Priority
     Mid,
     Low
 }
+
+/// <summary>
+/// Class containing helper methods for implementing <see cref="ICheck"/>s.
+/// </summary>
+internal static class CheckHelper
+{
+    /// <summary>
+    /// Converts an <see cref="INode"/> to <see cref="T"/> and throws an exception if this is not possible.
+    /// </summary>
+    /// <param name="node">The <see cref="INode"/> to convert.</param>
+    /// <returns>The <see cref="INode"/> instance converted to an instance of type <see cref="T"/>.</returns>
+    /// <exception cref="IncorrectNodeTypeException">Thrown if <paramref name="node"/> is not of type <see cref="T"/></exception>
+    internal static T ConvertAndThrowIfIncorrectNodeType< T >(
+        INode node)
+        where T : INode => node is not T asT
+        ? throw IncorrectNodeTypeException.From< T >()
+        : asT;
+}
+
+/// <summary>
+/// Represents an error thrown when receiving an incorrect type of <see cref="INode"/> in a check.
+/// </summary>
+internal sealed class IncorrectNodeTypeException : Exception
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IncorrectNodeTypeException"/> class for the given <paramref name="type"/>
+    /// </summary>
+    /// <param name="type">The type to which the instance could not be converted.</param>
+    private IncorrectNodeTypeException(
+        Type type)
+        : base($"Node must be of type '{type}'")
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IncorrectNodeTypeException"/> class for the given <see cref="T"/>.
+    /// </summary>
+    internal static IncorrectNodeTypeException From< T >() => new( typeof( T ) );
+}
