6dcb4a6cf2df9761d9edc6a496244ddb73176634
Rutger <rutger.vincken@ziggo.nl>
2023-04-20 17:38:03 +0200

    Rec: Interfacecheck global setup

--- PatternPal/PatternPal.Core/Checks/InterfaceCheck.cs
+++ PatternPal/PatternPal.Core/Checks/InterfaceCheck.cs
@@ -1,18 +1,72 @@
-﻿namespace PatternPal.Core.Checks;
- internal class InterfaceCheck : CheckBase
- {
-     private readonly IEnumerable<ICheck> _checks;
+﻿using Microsoft.CodeAnalysis.VisualBasic.Syntax;
 
-     internal InterfaceCheck(Priority priority, 
-         IEnumerable<ICheck> checks) : base(priority)
-     {
-         _checks = checks;
-     }
+namespace PatternPal.Core.Checks;
+internal class InterfaceCheck : CheckBase
+{
+    private readonly IEnumerable<ICheck> _checks;
+
+    internal InterfaceCheck(Priority priority,
+        IEnumerable<ICheck> checks) : base(priority)
+    {
+        _checks = checks;
+    }
 
     public override ICheckResult Check(
          RecognizerContext ctx,
          INode node)
-     {
-        throw new NotImplementedException("Interface Check was not implemented");
+    {
+        IInterface interfaceEntity = CheckHelper.ConvertNodeElseThrow<IInterface>(node);
+
+
+        bool hasFailedSubCheck = false;
+        IList<ICheckResult> subCheckResults = new List<ICheckResult>();
+        foreach(ICheck check in _checks)
+        {
+            ICheckResult checkResult;
+            switch(check)
+            {
+                case ModifierCheck modifierCheck:
+                    {
+                        checkResult = modifierCheck.Check(ctx, interfaceEntity);
+                        break;
+                    }
+                case MethodCheck methodCheck:
+                    {
+
+                        break;
+                    }
+                case FieldCheck fieldCheck:
+                    {
+                        break;
+                    }
+                case ConstructorCheck constructorCheck:
+                    {
+                        break;
+                    }
+                case NotCheck notCheck:
+                    {
+                        break;
+                    }
+                case UsesCheck usesCheck:
+                    {
+                        break;
+                    }
+                default:
+                    {
+                        throw CheckHelper.InvalidSubCheck(this, check);
+                    }
+            }
+
+            subCheckResults.Add(checkResult);
+            if (!checkResult.Correctness)
+                hasFailedSubCheck = true;
+        }
+        return new NodeCheckResult
+        {
+            ChildrenCheckResults = subCheckResults,
+            Correctness = !hasFailedSubCheck,
+            FeedbackMessage = $"Found class '{interfaceEntity}'",
+            Priority = Priority,
+        };
     }
- }
+}
