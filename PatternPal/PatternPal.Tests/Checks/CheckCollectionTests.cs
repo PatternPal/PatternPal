5da0366cb2a98585fda3de319fcff9183568b410
Casper Verhaar <casper.verhaar@gmail.com>
2023-04-25 10:57:25 +0200

    Rec+Tests: Implement and test CheckCollection

---
+++ PatternPal/PatternPal.Tests/Checks/CheckCollectionTests.cs
@@ -0,0 +1,28 @@
+﻿#region
+
+using static PatternPal.Core.Checks.CheckBuilder;
+
+#endregion
+
+namespace PatternPal.Tests.Checks;
+
+[TestFixture]
+public class CheckCollectionTests
+{
+    [Test]
+    public Task Check_Collection_Returns_Correct_Result()
+    {
+        IClass classEntity = EntityNodeUtils.CreateClass();
+
+        CheckCollection checkCollection = Any(
+            Priority.Low,
+            Class(Priority.Low));
+        RecognizerContext ctx = new();
+
+        ICheckResult result = checkCollection.Check(
+            ctx,
+            classEntity);
+
+        return Verifier.Verify(result);
+    }
+}
