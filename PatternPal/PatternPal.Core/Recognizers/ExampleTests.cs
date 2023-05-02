7306da96d5461a9624c50ed68fc42b7e4d33295e
Rutger <rutger.vincken@ziggo.nl>
2023-05-02 18:42:36 +0200

    Rec: Refactor singleton
    allmost all requirements done, 3 TODO's left

---
+++ PatternPal/PatternPal.Core/Recognizers/ExampleTests.cs
@@ -0,0 +1,90 @@
+﻿#region
+
+using static PatternPal.Core.Checks.CheckBuilder;
+
+#endregion
+
+namespace PatternPal.Core.Recognizers;
+
+internal class ExampleTests : IRecognizer
+{
+    internal IEnumerable< ICheck > Create()
+    {
+        //MethodCheck instanceMethod =
+        //    Method(
+        //        Priority.Low,
+        //        Modifiers(
+        //            Priority.Low,
+        //            Modifier.Static
+        //        ),
+        //        Not(
+        //            Priority.Low,
+        //            Modifiers(
+        //                Priority.Low,
+        //                Modifier.Private
+        //            )
+        //        )
+        //    );
+
+        yield return Class(
+            Priority.Low,
+            instanceMethod,
+            Field(
+                Priority.Low,
+                Modifiers(
+                    Priority.Low,
+                    Modifier.Static,
+                    Modifier.Private
+                ),
+                Type(
+                    Priority.Low,
+                    ICheck.GetCurrentEntity
+                )
+            ),
+            //Constructor(
+            //    Priority.Low,
+            //    Modifiers(
+            //        Priority.Low,
+            //        Modifier.Private
+            //    )
+            //),
+            //Not(
+            //    Priority.Low,
+            //    Any(
+            //        Priority.Low,
+            //        Constructor(
+            //            Priority.Low,
+            //            Modifiers(
+            //                Priority.Low,
+            //                Modifier.Public
+            //            )
+            //        ),
+            //        Constructor(
+            //            Priority.Low,
+            //            Modifiers(
+            //                Priority.Low,
+            //                Modifier.Internal
+            //            )
+            //        ),
+            //        Constructor(
+            //            Priority.Low,
+            //            Modifiers(
+            //                Priority.Low,
+            //                Modifier.Protected
+            //            )
+            //        )
+            //    )
+            )
+        );
+
+        //yield return Class(
+        //    Priority.Low,
+        //    Method(
+        //        Priority.Low,
+        //        Uses(
+        //            Priority.Low,
+        //            instanceMethod.Result)
+        //    )
+        //);
+    }
+}
--- PatternPal/PatternPal.Core/Recognizers/SingletonRecognizer.cs
+++ PatternPal/PatternPal.Core/Recognizers/SingletonRecognizer.cs
@@ -1,6 +1,7 @@
 ﻿#region
 
 using static PatternPal.Core.Checks.CheckBuilder;
+using Type = System.Type;
 
 #endregion
 
@@ -8,83 +9,116 @@ namespace PatternPal.Core.Recognizers;
 
 internal class SingletonRecognizer : IRecognizer
 {
+    /* Requirements to fullfill the pattern:
+     *         Singleton
+     *               a) has no public/internal constructor
+     *               b) has at least one private/protected constructor
+     *               c) has a static, private field with the same type as the class
+     *               d) has a static, public/internal method that acts as a constructor in the following way
+     *                     1) if called and there is no instance saved in the private field, then it calls the private constructor
+     *                     2) if called and there is an instance saved in the private field it returns this instance
+     *         Client
+     *               a) calls the getInstance() method of the singleton class
+     */
+
     internal IEnumerable< ICheck > Create()
     {
-        MethodCheck instanceMethod =
-            Method(
-                Priority.Low,
-                Modifiers(
-                    Priority.Low,
-                    Modifier.Static
-                ),
-                Not(
-                    Priority.Low,
-                    Modifiers(
-                        Priority.Low,
-                        Modifier.Private
-                    )
-                )
-            );
-
-        yield return Class(
-            Priority.Low,
-            instanceMethod,
-            Field(
-                Priority.Low,
-                Modifiers(
-                    Priority.Low,
-                    Modifier.Static,
-                    Modifier.Private
-                ),
-                Type(
-                    Priority.Low,
-                    ICheck.GetCurrentEntity
-                )
-            ),
+        //Singleton a) has no public/internal constructor
+        NotCheck checkSingletonA = Not(
+            Priority.Knockout,
             Constructor(
-                Priority.Low,
-                Modifiers(
-                    Priority.Low,
-                    Modifier.Private
-                )
-            ),
-            Not(
-                Priority.Low,
+                Priority.Knockout,
                 Any(
-                    Priority.Low,
-                    Constructor(
-                        Priority.Low,
-                        Modifiers(
-                            Priority.Low,
-                            Modifier.Public
-                        )
-                    ),
-                    Constructor(
-                        Priority.Low,
-                        Modifiers(
-                            Priority.Low,
-                            Modifier.Internal
-                        )
-                    ),
-                    Constructor(
-                        Priority.Low,
-                        Modifiers(
-                            Priority.Low,
-                            Modifier.Protected
-                        )
+                    Priority.Knockout,
+                    Modifiers(
+                        Priority.Knockout,
+                        Modifier.Public),
+                    Modifiers(
+                        Priority.Knockout,
+                        Modifier.Internal
                     )
                 )
             )
         );
 
-        yield return Class(
-            Priority.Low,
-            Method(
-                Priority.Low,
-                Uses(
-                    Priority.Low,
-                    instanceMethod.Result)
+        //Singleton b) has at least one private/protected constructor
+        ConstructorCheck checkSingletonB = Constructor(
+            Priority.Knockout,
+            Any(
+                Priority.Knockout,
+                Modifiers(
+                    Priority.Knockout,
+                    Modifier.Private
+                ),
+                Modifiers(
+                    Priority.Knockout,
+                    Modifier.Protected
+                )
             )
         );
+
+        //Singleton c) has a static, private field with the same type as the class
+        FieldCheck checkSingletonC = Field(
+            Priority.Knockout,
+            Modifiers(
+                priority.knockout,
+                Modifier.Static,
+                Modifier.Private
+            ),
+            Type(
+                Priority.Knockout,
+               //TODO GEEN IDEE OF DIT KLOPT!
+               ICheck.GetCurrentEntity
+            )
+        );
+
+        //Singleton d0) has a static, public/internal method
+        MethodCheck getInstanceMethod = Method(
+            Priority.Knockout,
+            Modifiers(
+                Priority.Low,
+                Modifier.Static
+            ),
+            Any(
+                Priority.Low,
+                Modifiers(
+                    Priority.Low,
+                    Modifier.Public
+                ),
+                Modifiers(
+                    Priority.Mid,
+                    Modifier.Internal
+                )
+            )
+        );
+
+        //Singleton d1) if called and there is no instance saved in the private field, then it calls the private constructor
+        //TODO: implement
+
+        //Singleton d2) if called and there is an instance saved in the private field it returns this instance
+        //TODO: implement
+
+        //Client a) calls the getInstance() method of the singleton class
+        MethodCheck checkClientA = Method(
+            Priority.Mid,
+            Uses(
+                Priority.Mid,
+                getInstanceMethod.Result
+            )
+        );
+
+        yield return Class(
+            Priority.Low,
+            checkSingletonA,
+            checkSingletonB,
+            checkSingletonC,
+            getInstanceMethod
+        );
+
+        yield return Class(
+            Priority.Low,
+            checkClientA
+        );
+
     }
 }
