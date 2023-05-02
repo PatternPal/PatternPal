b1bd1c89a39027e86962ba9119d6cf72ce964ce6
Siem van den Tweel <siemvdtweel@gmail.com>
2023-05-02 10:53:08 +0200

    log: Add comments to entire logging server

--- PatternPal/PatternPal.LoggingServer/Data/EventRepository.cs
+++ PatternPal/PatternPal.LoggingServer/Data/EventRepository.cs
@@ -12,17 +12,30 @@ namespace PatternPal.LoggingServer.Data
         {
             _context = context;
         }
-
+        /// <summary>
+        /// Returns list of all events
+        /// </summary>
+        /// <returns></returns>
         public async Task<List<ProgSnap2Event>> GetAll()
         {
             return await _context.Events.ToListAsync();
         }
 
+        /// <summary>
+        /// Returns event by id or null if not found
+        /// </summary>
+        /// <param name="id">ID of Progsnap2Event</param>
+        /// <returns> Event or null if not found </returns>
         public async Task<ProgSnap2Event?> GetById(string id)
         {
             return await _context.Events.FindAsync(id);
         }
 
+        /// <summary>
+        /// Inserts event into database asynchronously and returns the inserted event.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
         public async Task<ProgSnap2Event> Insert(ProgSnap2Event entity)
         {
             await _context.Events.AddAsync(entity);
@@ -30,6 +43,11 @@ namespace PatternPal.LoggingServer.Data
             return entity;
         }
 
+        /// <summary>
+        /// Updates event in database asynchronously and returns the updated event.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns>ProgSnap2Event</returns>
         public async Task<ProgSnap2Event> Update(ProgSnap2Event entity)
         {
             _context.Events.Update(entity);
@@ -37,7 +55,12 @@ namespace PatternPal.LoggingServer.Data
             return entity;
         }
 
-        public async Task<ProgSnap2Event> Delete(string id)
+        /// <summary>
+        /// Delete event from database asynchronously by id and returns void. If entity is not found, throws exception.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <exception cref="Exception"></exception>
+        public async Task Delete(string id)
         {
             ProgSnap2Event? entity = await _context.Events.FindAsync(id);
 
@@ -47,19 +70,40 @@ namespace PatternPal.LoggingServer.Data
             }
             _context.Events.Remove(entity);
             await _context.SaveChangesAsync();
-            return entity;
         }
 
+        /// <summary>
+        /// Generic method to get event by column name and value. This can be used to get events by any column instead of just id. Only caveat is that the column name must be case-sensitive and it must be a string.
+        /// </summary>
+        /// <param name="column">Column name (Case-Sensitive) </param>
+        /// <param name="value">Column value</param>
+        /// <returns cref="ProgSnap2Event?"></returns>
         public async Task<ProgSnap2Event?> GetBy(string column, string value)
         {
             return await _context.Events.FirstOrDefaultAsync(e => EF.Property<string>(e, column) == value);
         }
 
+        /// <summary>
+        /// Same filter as GetBy, but returns a list of all matching events.
+        /// TODO: This is a temporary solution. We should be able to filter by any column, not just string columns.
+        /// TODO: Limit, Offset to prevent large queries
+        /// </summary>
+        /// <param name="column">Column name (Case-Sensitive) </param>
+        /// <param name="value">Column value</param>
+        /// <returns></returns>
         public async Task<List<ProgSnap2Event>> GetAllBy(string column, string value)
         {
             return await _context.Events.Where(e => EF.Property<string>(e, column) == value).ToListAsync();
         }
 
+
+        /// <summary>
+        /// To get correct event order, we need to get the last event in the session and subject and increment the order by 1.
+        /// In case there are no events in the session and subject, we return 0.
+        /// </summary>
+        /// <param name="sessionId"></param>
+        /// <param name="subjectId"></param>
+        /// <returns cref="int"></returns>
         public async Task<int> GetNextOrder(Guid sessionId, Guid subjectId)
         {
             ProgSnap2Event? lastEvent = await _context.Events.Where(e => e.SessionId == sessionId && e.SubjectId == subjectId).OrderByDescending(e => e.Order).FirstOrDefaultAsync();
