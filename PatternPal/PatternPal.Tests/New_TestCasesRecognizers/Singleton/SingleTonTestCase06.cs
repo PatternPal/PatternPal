682f70a6bcbd6cf5ef721b7a3e54b0e84fbc2f58
Rutger <rutger.vincken@ziggo.nl>
2023-05-09 14:48:39 +0200

    Tests: Singleton requirements changed

--- PatternPal/PatternPal.Tests/New_TestCasesRecognizers/Singleton/SingleTonTestCase06.cs
+++ PatternPal/PatternPal.Tests/New_TestCasesRecognizers/Singleton/SingleTonTestCase06.cs
@@ -14,8 +14,7 @@
      *                     1) if called and there is no instance saved in the private field, then it calls the private constructor
      *                     2) if called and there is an instance saved in the private field it returns this instance
      *         Client
-     *               a) the first call of the getInstance() of the Singleton class returns a new instance of this class
-     *               b) the second and next calls of getInstance() of the Singleton class return the same instance of the Singleton class.
+     *               a) calls the getInstance() method of the singleton class
      */
     public class SingleTonTestCase06
     {
