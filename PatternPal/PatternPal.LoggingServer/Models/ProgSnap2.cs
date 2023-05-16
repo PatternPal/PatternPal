3d76db107fdb4fa5e47d0acb06ae6150186a14a1
Nireon <siemvdtweel@gmail.com>
2023-05-16 11:21:39 +0200

    Log: Add comments to entire logging server (#204)

--- PatternPal/PatternPal.LoggingServer/Models/ProgSnap2.cs
+++ PatternPal/PatternPal.LoggingServer/Models/ProgSnap2.cs
@@ -6,32 +6,85 @@ using PatternPal.LoggingServer.Models;
 using PatternPal.LoggingServer;
 namespace PatternPal.LoggingServer.Models
 {
+
+    /// <summary>
+    /// Represents an event in ProgSnap2, including information about the session, subject, tool instances, code state, and timing.
+    /// </summary>
     public class ProgSnap2Event
     {
+        /// <summary>
+        /// The primary key for the event. This must be unique.
+        /// </summary>
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid EventId { get; set; }
 
+        /// <summary>
+        /// The order of the event within the session, starting at 0.
+        /// </summary>
         public int Order { get; set; }
+
+        /// <summary>
+        /// The ID of the session that this event belongs to. Every extension launch is a new session.
+        /// </summary>
         public Guid SessionId { get; set; }
+
+        /// <summary>
+        /// The ID of the subject that this event belongs to. Every new extension installation is a new subject.
+        /// </summary>
         public Guid SubjectId { get; set; }
+
+        /// <summary>
+        /// Information about the IDE, compiler, OS, programming language, and other tool instances.
+        /// </summary>
         public string ToolInstances { get; set; }
+
+        /// <summary>
+        /// The ID of the code state that this event refers to.
+        /// </summary>
         public Guid CodeStateId { get; set; }
+
+        /// <summary>
+        /// The type of event.
+        /// </summary>
         public EventType EventType { get; set; }
 
+        /// <summary>
+        /// The date and time of the event, in the server's time zone.
+        /// </summary>
+        public DateTimeOffset ServerDatetime { get; set; }
+
+        /// <summary>
+        /// The date and time of the event, in the client's time zone.
+        /// </summary>
+        public DateTimeOffset ClientDatetime { get; set; }
+        /// <summary>
+        /// The ID of the parent event, if any. Used in cases such as compile.error, where the parent event is compile.
+        /// </summary>
         public Guid? ParentId { get; set; }
 
+        /// <summary>
+        /// The ID of the project associated. This is generated per project, and is the same for all events in the same project.
+        /// </summary>
         public string? ProjectId { get; set; }
 
-        public string? CompileMessageType { get; set; } 
+        /// <summary>
+        /// The type of compile message, such as "error", "warning", or "message".
+        /// </summary>
+        public string? CompileMessageType { get; set; }
 
+        /// <summary>
+        /// The content of the compile message.
+        /// </summary>
         public string? CompileMessage { get; set; }
+
+        /// <summary>
+        /// The source location associated with this compile event, if any.
+        /// </summary>
         public string? SourceLocation { get; set; }
 
+        /// <summary>
+        /// The section of the code state associated with this compile event. For example in compile.error, this would be the section of code that caused the error.
+        /// </summary>
         public string? CodeStateSection { get; set; }
-
-        public DateTimeOffset ServerDatetime { get; set; }
-        public DateTimeOffset ClientDatetime { get; set; }
-
     }
-
 }
--- PatternPal/PatternPal.LoggingServer/Services/LoggerService.cs
+++ PatternPal/PatternPal.LoggingServer/Services/LoggerService.cs
@@ -11,14 +11,26 @@ namespace PatternPal.LoggingServer.Services
     {
         private readonly ILogger<LoggerService> _logger;
         private readonly EventRepository _eventRepository;
+        
+        /// <summary>
+        /// Constructor for the LoggerService. This service is responsible for logging events to the database. If any other helper classes are needed, they should be added here as well using dependency injection.
+        /// </summary>
+        /// <param name="logger">Logger for the service class. Currently this follows the default ASP.NET Core logging configuration, but this can be changed in the Startup.cs file. </param>
+        /// <param name="repository"> The repository class that is responsible for communicating with the database. </param>
         public LoggerService(ILogger<LoggerService> logger, EventRepository repository)
         {
             _logger = logger;
             _eventRepository = repository;
         }
 
-
-
+        /// <summary>
+        /// The main logging method. This method is called by the client to log an event. It's a one-way method, so the client doesn't need to wait for a response and will not return any information.
+        /// Furthermore since there is no validation in gRPC, we need to do it manually. This is done by checking the validity of the sessionID, subjectID, event type and the client timestamp. If any of these are invalid, an exception is thrown and the client will receive an error message.
+        /// </summary>
+        /// <param name="request">All information sent along from the Client required to add a new entry</param>
+        /// <param name="context">Server context required</param>
+        /// <returns cref="LogResponse">Confirmation</returns>
+        /// <exception cref="RpcException"></exception>
         public override async Task<LogResponse> Log(LogRequest request, ServerCallContext context)
         {
 
