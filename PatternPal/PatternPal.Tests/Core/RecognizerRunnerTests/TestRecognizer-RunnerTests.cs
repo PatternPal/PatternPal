871e117a5c3dedda904e906033c39d2a9127f810
Jeroen van Dam <jeroen.vandam@outlook.com>
2023-05-22 10:34:07 +0200

    Rec: Added a prune test, there are errors but VS doesn't show where

--- PatternPal/PatternPal.Tests/Core/RecognizerRunnerTests/TestRecognizer-RunnerTests.cs
+++ PatternPal/PatternPal.Tests/Core/RecognizerRunnerTests/TestRecognizer-RunnerTests.cs
@@ -1,4 +1,7 @@
-﻿using PatternPal.SyntaxTree;
+﻿using PatternPal.Core.Checks;
+using PatternPal.Core;
+using PatternPal.SyntaxTree;
+using static PatternPal.Core.RecognizerRunner;
 
 namespace PatternPal.Tests.Core.RecognizerRunnerTests;
 
@@ -6,6 +9,7 @@ namespace PatternPal.Tests.Core.RecognizerRunnerTests;
 internal class TestRecognizer_RunnerTests
 {
     [Test]
+    [TestCase("PruneTestClass1.cs", "lalal", true)]
     public void Knockout_Pruning(
         string filename,
         string node,
@@ -13,13 +17,37 @@ internal class TestRecognizer_RunnerTests
         )
     {
         //Create testrecognizer
-        string code = FileUtils.FileToString("Relation\\MethodsAndEntities\\" + filename);
-        string nameSpaceNode = "PatternPal.Tests.TestClasses.Relation.MethodsAndEntities";
+        string code = FileUtils.FileToString("PruneTests\\" + filename);
+        string nameSpaceNode = "PatternPal.Tests.TestClasses.PruneTests";
 
         SyntaxGraph graph = new();
         graph.AddFile(code, code);
         graph.CreateGraph();
 
+        TestRecognizer recognizer = new();
+        ICheck rootCheck = recognizer.CreateRootCheck();
+
+        IRecognizerContext ctx = new RecognizerContext
+        {
+            Graph = graph,
+            CurrentEntity = null!,
+            ParentCheck = rootCheck,
+        };
+
+        NodeCheckResult rootResult = (NodeCheckResult)rootCheck.Check(
+            ctx,
+            new RootNode());
+
+        SortCheckResults(rootResult);
+
+        // Filter the results.
+        Dictionary<INode, List<ICheckResult>> resultsByNode = new();
+        FilterResults(
+            resultsByNode,
+            rootResult);
+
+
+        Assert.AreEqual(shouldBePruned, true);
 
         //Test if name of node is in result
         //Assert shouldBePruned
---
+++ PatternPal/PatternPal.Tests/TestClasses/PruneTests/PruneTestClass1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PatternPal.Tests.TestClasses.Strategy;
+
+namespace PatternPal.Tests.TestClasses.PruneTests
+{
+    internal class PruneTestClass1
+    {
+        public static int CalculateClient()
+        {
+            return 1 + 1;
+        }
+    }
+
+    internal class ClassUsesPruneClass 
+    {
+        public int pizza = PruneTestClass1.CalculateClient();
+    }
+}
