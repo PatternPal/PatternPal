02a968e73210236c3b0d913475547a3e4d0a3851
DanielvanDamme <69575357+DanielvanDamme@users.noreply.github.com>
2023-06-12 12:56:42 +0200

    SBS: Implemented SBS for singleton.

--- PatternPal/PatternPal.Core/Recognizers/SingletonRecognizer.cs
+++ PatternPal/PatternPal.Core/Recognizers/SingletonRecognizer.cs
@@ -66,9 +66,14 @@ internal class SingletonRecognizer : IRecognizer,
     /// <inheritdoc />
     List< IInstruction > IStepByStepRecognizer.GenerateStepsList()
     {
-        List< IInstruction > res = new List< IInstruction >();
+        List< IInstruction > res = new();
 
-        // !!! THESE STEPS ARE OUTDATED, JUST AN EXAMPLE !!!
+        ICheck onlyPrivateConstructorCheck =
+            OnlyPrivateProtectedConstructor(out ConstructorCheck privateConstructorCheck);
+        FieldCheck staticPrivateFieldOfTypeClass = StaticPrivateFieldOfTypeClass();
+        MethodCheck checkMethodAsConstructorBehaviour =
+            CheckMethodActsAsConstructorBehaviour(privateConstructorCheck, staticPrivateFieldOfTypeClass);
+
         // Step 1: The constructor is ONLY private
         res.Add(
             new SimpleInstruction(
@@ -76,16 +81,12 @@ internal class SingletonRecognizer : IRecognizer,
                 SingletonInstructions.Explanation1,
                 new List< ICheck >
                 {
-                    Class(
-                        Priority.Knockout,
-                        OnlyPrivateProtectedConstructor(
-                            out ConstructorCheck privateConstructorCheck))
+                    Class(Priority.Knockout,
+                        onlyPrivateConstructorCheck)
                 }
             ));
 
-        FieldCheck staticPrivateFieldOfTypeClass =
-            StaticPrivateFieldOfTypeClass();
-
+        
         // Step 2: There is a static private field with the same type as the class
         res.Add(
             new SimpleInstruction(
@@ -93,22 +94,39 @@ internal class SingletonRecognizer : IRecognizer,
                 SingletonInstructions.Explanation2,
                 new List< ICheck >
                 {
-                    Class(
-                        Priority.Knockout,
-                        All(
-                            Priority.Low,
+                    Class(Priority.Knockout,
+                        All(Priority.Low,
                             staticPrivateFieldOfTypeClass))
                 }
             ));
+        
 
-        MethodCheck checkMethodActsAsConstructorBehaviour = CheckMethodActsAsConstructorBehaviour(
-            privateConstructorCheck,
-            staticPrivateFieldOfTypeClass);
         // Step 3: There is a method that acts as the constructor
-        // TODO
+        res.Add(
+            new SimpleInstruction(
+                SingletonInstructions.Step3,
+                SingletonInstructions.Explanation3,
+                new List< ICheck >
+                {
+                    Class(Priority.Knockout,
+                        onlyPrivateConstructorCheck,
+                        staticPrivateFieldOfTypeClass,
+                        checkMethodAsConstructorBehaviour)
+                }));
 
         // Step 4: There is a client that calls the instance method
-        // TODO
+        res.Add(
+            new SimpleInstruction(
+                SingletonInstructions.Step4,
+                SingletonInstructions.Explanation4,
+                new List< ICheck >
+                {
+                    Class(Priority.Knockout,
+                        onlyPrivateConstructorCheck,
+                        staticPrivateFieldOfTypeClass,
+                        checkMethodAsConstructorBehaviour),
+                    ClientCallsMethodActsAsConstructor(checkMethodAsConstructorBehaviour)
+                }));
 
         return res;
     }
