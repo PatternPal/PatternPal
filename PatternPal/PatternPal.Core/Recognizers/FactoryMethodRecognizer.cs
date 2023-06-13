d348163c32aaf9573218e3fca6b3cc61bea9a68f
Rutger <rutger.vincken@ziggo.nl>
2023-06-13 18:25:36 +0200

    Rec: Splitted method in smaller methods

--- PatternPal/PatternPal.Core/Recognizers/FactoryMethodRecognizer.cs
+++ PatternPal/PatternPal.Core/Recognizers/FactoryMethodRecognizer.cs
@@ -17,14 +17,14 @@ namespace PatternPal.Core.Recognizers;
 /// Requirements for the Product class:<br/>
 ///     a) is an interface<br/>
 ///     b) gets inherited by at least one class<br/>
-///     c) gets inherited by at least two classes<br/>
+///     c) gets inherited by at least two classes<br/>                          --todo: check this
 /// Requirements for the Concrete Product class:<br/>
 ///     a) inherits Product<br/>
 ///     b) gets created in a Concrete Creator<br/>
 /// Requirements for the Creator class:<br/>
 ///     a) is an abstract class<br/>
 ///     b) gets inherited by at least one class<br/>
-///     c) gets inherited by at least two classes<br/>
+///     c) gets inherited by at least two classes<br/>                          --todo: check this
 ///     d) contains a factory-method with the following properties<br/>
 ///         1) method is abstract<br/>
 ///         2) method is public<br/>
@@ -45,31 +45,42 @@ internal class FactoryMethodRecognizer : IRecognizer
     /// A method which creates a lot of <see cref="ICheck"/>s that each adheres to the requirements a singleton pattern needs to have implemented.
     /// It returns the requirements in a tree structure stated per class.
     /// </summary>
-    public IEnumerable< ICheck > Create()
+    public IEnumerable<ICheck> Create()
     {
-        //Product c
-        //Creator c
+        InterfaceCheck product = Product();
 
+        ClassCheck creator = Creator(product);
+
+        ClassCheck concreteProduct = ConcreteProduct(product);
+
+        yield return product;
+        yield return concreteProduct;
+        yield return creator;
+        yield return ConcreteCreator(creator, concreteProduct);
+    }
+
+    InterfaceCheck Product()
+    {
         //Product a
-        InterfaceCheck productIsInterface = Interface(
-            Priority.Knockout
-        );
-
-
-        //Concrete product a & Product b
-        RelationCheck concreteInheritsProduct = Implements(
+        return Interface(
             Priority.Knockout,
-            productIsInterface);
+            "is an interface"
+        );
+    }
 
+    ClassCheck Creator(InterfaceCheck product)
+    {
         //Creator a
         ModifierCheck isAbstract = Modifiers(
             Priority.Knockout,
+            "is an abstract class",
             Modifier.Abstract
         );
 
         //Creator d
         MethodCheck factoryMethod = Method(
             Priority.High,
+            "contains a factory-method with the following properties: 1) method is abstract 2) method is public 3) returns an object of type Product",
             Modifiers(
                 Priority.Knockout,
                 Modifier.Abstract
@@ -80,32 +91,44 @@ internal class FactoryMethodRecognizer : IRecognizer
             ),
             Type(
                 Priority.Knockout,
-                productIsInterface
+                product
             )
         );
-        
-        ClassCheck creator = Class(
+
+        return Class(
             Priority.Low,
             isAbstract,
-            //b
-            //c
             factoryMethod
         );
+    }
 
+    ClassCheck ConcreteProduct(InterfaceCheck product)
+    {
+        //Concrete product a & Product b
+        RelationCheck concreteInheritsProduct = Implements(
+            Priority.Knockout,
+            "gets inherited by at least one class + Concrete Product inherits Product",
+            product);
+
+        return Class(
+            Priority.Low,
+            concreteInheritsProduct
+        );
+    }
+
+    ClassCheck ConcreteCreator(ClassCheck creator, ClassCheck concreteProduct)
+    {
         //Concrete Creator a & Creator b
         RelationCheck concreteInheritsCreator = Inherits(
             Priority.Knockout,
+            "inherits Creator + Creator gets inherited by at least one class",
             creator
-        );
-
-        ClassCheck concreteProduct = Class(
-            Priority.Low,
-            concreteInheritsProduct
         );
 
         //Concrete Creator b --Todo does not check if it is exactly one
         MethodCheck methodReturnsConcreteProduct = Method(
             Priority.Low,
+            "has exactly one method that creates and returns a Concrete product",
             Type(
                 Priority.Low,
                 concreteProduct
@@ -119,20 +142,16 @@ internal class FactoryMethodRecognizer : IRecognizer
         //Concrete product b
         RelationCheck createsConcreteProduct = Creates(
             Priority.High,
+            "gets created in a Concrete Creator",
             concreteProduct
         );
 
-        ClassCheck concreteCreator = Class(
+        return Class(
             Priority.Low,
             concreteInheritsCreator,
             methodReturnsConcreteProduct,
             createsConcreteProduct
         );
-
-        yield return productIsInterface;
-        yield return concreteProduct;
-        yield return creator;
-        yield return concreteCreator;
     }
 
     public List<IInstruction> GenerateStepsList()
