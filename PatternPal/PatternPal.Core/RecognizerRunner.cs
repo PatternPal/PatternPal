78c293b0cd42a1ea509e94d5a8352ece179e2b5a
Casper Verhaar <casper.verhaar@gmail.com>
2023-06-13 14:02:21 +0200

    Rec: Take RelationCheck score into account when sorting results

--- PatternPal/PatternPal.Core/RecognizerRunner.cs
+++ PatternPal/PatternPal.Core/RecognizerRunner.cs
@@ -209,7 +209,9 @@ public class RecognizerRunner
                 rootResult,
                 pruneAll);
 
-            PrioritySort(rootResult);
+            SortResultsByPriorities(
+                resultsByNode,
+                rootResult);
             return rootResult;
         }
     }
@@ -513,12 +515,15 @@ public class RecognizerRunner
     /// based their <see cref="Priority"/>s and the <see cref="Score"/>s and <see cref="Priority"/>s
     /// of their children.
     /// </summary>
-    internal static void PrioritySort(
+    internal static void SortResultsByPriorities(
+        IReadOnlyDictionary< INode, List< ICheckResult > > resultsByNode,
         NodeCheckResult result)
     {
         foreach (ICheckResult childResult in result.ChildrenCheckResults)
         {
-            PrioritySortHelper(childResult);
+            SortResultsByPrioritiesImpl(
+                resultsByNode,
+                childResult);
             result.Score += childResult.Score;
         }
 
@@ -526,44 +531,122 @@ public class RecognizerRunner
             (
                 a,
                 b) => a.Score.CompareTo(b.Score));
-    }
 
-    /// <summary>
-    /// Sorts and determines the <see cref="Score"/> of the childResult based on the type of childResult.
-    /// </summary>
-    private static void PrioritySortHelper(
-        ICheckResult childResult)
-    {
-        switch (childResult)
+        static void SortResultsByPrioritiesImpl(
+            IReadOnlyDictionary< INode, List< ICheckResult > > resultsByNode,
+            ICheckResult childResult)
         {
-            case LeafCheckResult leafResult:
-                leafResult.Score = Score.CreateScore(
-                    leafResult.Priority,
-                    leafResult.Correct);
-                //TODO add Relation and TypeCheck. Idea underneath. However not as straightforward since a RelationCheck and a TypeCheck are NodeCheckResults
-                /*if (leafResult is { Correct: true, Check: RelationCheck or TypeCheck })
+            switch (childResult)
+            {
+                case LeafCheckResult leafResult:
                 {
-                    // Get the CheckResults belonging to the related node.
-                    List<ICheckResult> resultsOfNode = resultsByNode[leafResult.MatchedNode];
+                    leafResult.Score = Score.CreateScore(
+                        leafResult.Priority,
+                        leafResult.Correct);
 
-                    // Find the CheckResult linked to the check which searched for the related node.
-                    ICheckResult? relevantResult = resultsOfNode.FirstOrDefault(
-                        result => result.Check == leafResult.RelatedCheck);
+                    // Take into account the score of the related node if this result belongs to a
+                    // Relation- or TypeCheck.
+                    if (leafResult is {Correct: true, Check: RelationCheck or TypeCheck, MatchedNode: not null})
+                    {
+                        // Get the CheckResults belonging to the related node.
+                        List< ICheckResult > resultsOfNode = resultsByNode[ leafResult.MatchedNode ];
 
-                    leafResult.Score += relevantResult.Score;
-                }*/
-                break;
-            case NodeCheckResult nodeResult:
-                PrioritySort(nodeResult);
-                break;
-            case NotCheckResult notResult:
-                PrioritySortHelper(notResult.NestedResult);
-                notResult.Score = Score.GetNot(
-                    notResult.NestedResult.Priority,
-                    notResult.NestedResult.Score);
-                break;
-            default:
-                throw new ArgumentException($"{childResult} is not a supported ICheckResult");
+                        // Find the CheckResult linked to the check which searched for the related node.
+                        ICheckResult ? relevantResult = resultsOfNode.FirstOrDefault(
+                            result => result.Check == leafResult.RelatedCheck);
+
+                        if (relevantResult == null)
+                        {
+                            // Probably unreachable.
+                            break;
+                        }
+
+                        // Increment the score with the score of the related node. If the related
+                        // node is not an entity, take the score of its entity parent. Otherwise,
+                        // correctly implemented members in bad parents would weigh more than poorly
+                        // implemented members in good parents.
+                        leafResult.Score += leafResult.MatchedNode switch
+                        {
+                            IEntity => relevantResult.Score,
+                            IMember member => GetParentScoreFromMember(
+                                resultsByNode,
+                                relevantResult,
+                                member),
+                            _ => throw new UnreachableException("Relation to non-IMember or IEntity")
+                        };
+
+                        // Gets the score of the `member`s parent of type Entity.
+                        static Score GetParentScoreFromMember(
+                            IReadOnlyDictionary< INode, List< ICheckResult > > resultsByNode,
+                            ICheckResult relevantResult,
+                            IMember member)
+                        {
+                            IEntity parent = member.GetParent();
+
+                            foreach (ICheckResult parentResult in resultsByNode[ parent ])
+                            {
+                                if (parentResult is LeafCheckResult)
+                                {
+                                    continue;
+                                }
+
+                                if (HasDescendantResult(
+                                    parentResult,
+                                    relevantResult))
+                                {
+                                    return parentResult.Score;
+                                }
+                            }
+
+                            return default;
+
+                            // Checks if `relevantResult` is a descendant of `parentResult`.
+                            static bool HasDescendantResult(
+                                ICheckResult parentResult,
+                                ICheckResult relevantResult)
+                            {
+                                if (parentResult == relevantResult)
+                                {
+                                    return true;
+                                }
+
+                                switch (parentResult)
+                                {
+                                    case LeafCheckResult:
+                                        return false;
+                                    case NotCheckResult notCheckResult:
+                                        return HasDescendantResult(
+                                            notCheckResult.NestedResult,
+                                            relevantResult);
+                                    case NodeCheckResult nodeCheckResult:
+                                        return nodeCheckResult.ChildrenCheckResults.Any(
+                                            childResult => HasDescendantResult(
+                                                childResult,
+                                                relevantResult));
+                                    default:
+                                        throw new ArgumentException($"Unsupported CheckResult type: {parentResult.GetType()}");
+                                }
+                            }
+                        }
+                    }
+                    break;
+                }
+                case NodeCheckResult nodeResult:
+                    SortResultsByPriorities(
+                        resultsByNode,
+                        nodeResult);
+                    break;
+                case NotCheckResult notResult:
+                    SortResultsByPrioritiesImpl(
+                        resultsByNode,
+                        notResult.NestedResult);
+                    notResult.Score = Score.GetNot(
+                        notResult.NestedResult.Priority,
+                        notResult.NestedResult.Score);
+                    break;
+                default:
+                    throw new ArgumentException($"{childResult} is not a supported ICheckResult");
+            }
         }
     }
 }
