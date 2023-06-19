0dcee09971a9a0fa049e6754f65cfa21653220c4
Olaf van der Aart <ovda96@gmail.com>
2023-06-20 00:53:42 +0200

    Log: More arguments, JSON-validation

--- PatternPal/PatternPal.ProgsnapExport/Program.cs
+++ PatternPal/PatternPal.ProgsnapExport/Program.cs
@@ -1,6 +1,7 @@
 ﻿#region
 
 using System.ComponentModel;
+using System.Text.Json;
 
 using Spectre.Console.Cli;
 using Spectre.Console;
@@ -9,14 +10,17 @@ using Spectre.Console;
 
 namespace PatternPal.ProgSnapExport;
 
+// ReSharper disable once ClassNeverInstantiated.Global
 internal sealed class ProgSnapExportCommand : Command<ProgSnapExportCommand.Settings>
 {
+    // ReSharper disable once ClassNeverInstantiated.Global
     public sealed class Settings : CommandSettings
     {
         // TODO Add validation
-        [Description("Path to config file; defaults to \"./settings.json\".")]
+        [Description("Path to config file; defaults to \"./config.json\".")]
         [CommandArgument(0, "[configFile]")]
-        [DefaultValue("./settings.json")]
+        [DefaultValue("./config.json")]
+        // ReSharper disable once 
         public string ConfigFile { get; init; }
 
         [Description("Path to export directory; defaults to \"./export\".")]
@@ -33,28 +37,67 @@ internal sealed class ProgSnapExportCommand : Command<ProgSnapExportCommand.Sett
         [Description("Specifies the ending point of the time interval to be exported.")]
         [CommandOption("-e|--end")]
         public string? EndInterval { get; init; }
+        
+        [CommandOption("--csv-only")]
+        [DefaultValue(false)]
+        public bool CsvOnly { get; init; }
 
         [CommandOption("-v|--verbose")]
         [DefaultValue(false)]
         public bool Verbose { get; init; }
 
+        public string ConnectionString { get; private set; }
+
         // TODO Add storage option for online
 
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <returns></returns>
         public override ValidationResult Validate()
         {
-            base.Validate();
+            ValidationResult baseValidationResult = base.Validate();
 
-            return File.Exists(ConfigFile)
-                ? ValidationResult.Success()
-                : ValidationResult.Error("Invalid path to config file specified");
+            if (!baseValidationResult.Successful)
+            {
+                return baseValidationResult;
+            }
+
+            // Config file
+            {
+                if (!File.Exists(ConfigFile))
+                {
+                    return ValidationResult.Error("Invalid path to config file specified");
+                }
+
+                try
+                {
+                    Dictionary<string, string> config =
+                        JsonSerializer.Deserialize<Dictionary<string, string>>(File.ReadAllText(ConfigFile));
+                    ConnectionString =
+                        $"Server={config["ServerUrl"]};Username={config["Username"]};Database={config["Database"]};Password={config["Password"]}";
+                }
+                catch
+                {
+                    return ValidationResult.Error("Invalid JSON specified");
+                }
+            }
             
             // TODO Add timestamp validation
+            
+            return ValidationResult.Success();
         }
     }
 
+    /// <summary>
+    /// TODO
+    /// </summary>
+    /// <param name="context"></param>
+    /// <param name="settings"></param>
+    /// <returns></returns>
     public override int Execute(CommandContext context, Settings settings)
     {
-        Console.WriteLine(settings.ConfigFile);
+        Console.WriteLine(settings.ConnectionString);
         return 0;
     }
 }
