a417218b100b60a1d97f2f7f369eb58c44e1bb75
Siem van den Tweel <siemvdtweel@gmail.com>
2023-06-19 14:38:28 +0200

    Log: Add migration for new column

---
+++ PatternPal/PatternPal.LoggingServer/Migrations/20230619123815_AddOldFileNameColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PatternPal.LoggingServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOldFileNameColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OldFileName",
+                table: "Events",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OldFileName",
+                table: "Events");
+        }
+    }
+}
