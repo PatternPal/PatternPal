93c681a37132a6fb27248b25a6241c27b721fa76
Olaf van der Aart <ovda96@gmail.com>
2023-06-20 16:04:44 +0200

    Log: Began codeState restore implementation

--- PatternPal/PatternPal.ProgsnapExport/Program.cs
+++ PatternPal/PatternPal.ProgsnapExport/Program.cs
@@ -12,6 +12,7 @@ using Spectre.Console;
 
 using CsvHelper;
 using CsvHelper.Configuration;
+using PatternPal.LoggingServer;
 
 #endregion
 
@@ -25,7 +26,7 @@ internal sealed class ProgSnapExportCommand : Command<ProgSnapExportCommand.Sett
 
     private string _logFile;
     private string _csvFile;
-    private string _codeStateExport;
+    private string _codeStateExportDir;
 
     /// <summary>
     /// Defines all CLI-arguments.
@@ -164,16 +165,16 @@ internal sealed class ProgSnapExportCommand : Command<ProgSnapExportCommand.Sett
     }
 
     /// <summary>
-    /// TODO
+    /// Defines the main program logic.
     /// </summary>
     /// <param name="context"></param>
     /// <param name="settings"></param>
-    /// <returns></returns>
+    /// <returns>The application's exit code</returns>
     public override int Execute(CommandContext context, Settings settings)
     {
         if (!SetUp(settings))
         {
-            LogError("Failed connecting to the PostgreSQL-database");
+            LogError("Failed connecting to the PostgreSQL-database.");
             return 105; // https://github.com/dotnet/templating/wiki/Exit-Codes
         }
 
@@ -182,9 +183,8 @@ internal sealed class ProgSnapExportCommand : Command<ProgSnapExportCommand.Sett
         //  2) If a bound is specified, check if it's not later in the future than now.
 
         // Obtain distinct sessionIds
-        // Note that we order by ClientDateTime to maintain that ordening
-        // in the eventual .csv-export.
-        LogInfo("Obtaining distinct sessionIDs..");
+        // Note that we order by ClientDateTime to maintain that ordening in the eventual .csv-export.
+        LogInfo("Obtaining distinct sessionIDs...");
         List<Guid> sessions = _dbContext.Events
             .OrderBy(e => e.ClientDatetime)
             .Select(e => e.SessionId)
@@ -219,7 +219,7 @@ internal sealed class ProgSnapExportCommand : Command<ProgSnapExportCommand.Sett
         _settings = settings;
         _csvFile = Path.Join(settings.ExportDirectory, "data.csv");
         _logFile = Path.Join(settings.ExportDirectory, "log.txt");
-        _codeStateExport = Path.Join(settings.ExportDirectory, "codestates");
+        _codeStateExportDir = Path.Join(settings.ExportDirectory, "codestates");
         
         // Create export directory
         if (_settings.ForceExportDirectory && Directory.Exists(_settings.ExportDirectory))
@@ -228,11 +228,9 @@ internal sealed class ProgSnapExportCommand : Command<ProgSnapExportCommand.Sett
         }
         Directory.CreateDirectory(_settings.ExportDirectory);
         
-        // Create log file
+        // Create log file, CodeState export directory
         using (FileStream fs = File.Create(_logFile)) ;
-        
-        // Create CodeState export directory
-        Directory.CreateDirectory(_codeStateExport);
+        Directory.CreateDirectory(_codeStateExportDir);
 
         // Set-up database
         _dbContext = new ProgSnap2ContextClass(_settings.ConnectionString);
@@ -248,21 +246,26 @@ internal sealed class ProgSnapExportCommand : Command<ProgSnapExportCommand.Sett
     /// <param name="sessionId"></param>
     private void ParseSession(Guid sessionId)
     {
-        LogInfo($"Parsing session [bold]{sessionId}[/]..");
-        LogInfo($"Obtaining data..");
+        // TODO Included markdown is currently showing in log file
+        LogInfo($"Parsing session [bold]{sessionId}[/]");
+        LogInfo($"Obtaining data...");
         List<ProgSnap2Event> data = _dbContext.Events
             .Where(e => e.SessionId == sessionId)
             .OrderBy(e => e.Order)
             .ToList();
         
-        LogInfo($"Writing data to csv..");
-        WriteDataToCsv(data, _csvFile);
-
         if (!_settings.CsvOnly)
         {
-            LogInfo($"Restoring CodeStates..");
-            RestoreCodeState();
+            // We first restore the codeStates since this process might add
+            // extra details to the eventual database.
+            LogInfo($"Restoring CodeStates...");
+            RestoreCodeState(ref data);
         }
+        
+        LogInfo($"Writing data to csv...");
+        WriteDataToCsv(data, _csvFile);
+
+        
     }
 
     /// <summary>
@@ -277,21 +280,21 @@ internal sealed class ProgSnapExportCommand : Command<ProgSnapExportCommand.Sett
         if (!Path.Exists(path))
         {
             // The csv-file was not created, and we are thus not appending.
-            using StreamWriter sw = new StreamWriter(path);
-            using CsvWriter csv = new CsvWriter(sw, CultureInfo.InvariantCulture);
+            using StreamWriter sw = new(path);
+            using CsvWriter csv = new(sw, CultureInfo.InvariantCulture);
             csv.WriteRecords(data);
         }
         else
         {
-            CsvConfiguration config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            CsvConfiguration config = new(CultureInfo.InvariantCulture)
             {
                 // If the file already existed, we should not re-include the header column of the *.csv-file.
                 HasHeaderRecord = false
             };
 
             using FileStream fs = File.Open(path, FileMode.Append);
-            using StreamWriter sw = new StreamWriter(fs);
-            using CsvWriter csv = new CsvWriter(sw, config);
+            using StreamWriter sw = new(fs);
+            using CsvWriter csv = new(sw, config);
             csv.WriteRecords(data);
         }
     }
@@ -299,9 +302,77 @@ internal sealed class ProgSnapExportCommand : Command<ProgSnapExportCommand.Sett
     /// <summary>
     /// TODO
     /// </summary>
-    private static void RestoreCodeState()
+    /// <param name="data"></param>
+    private void RestoreCodeState(ref List<ProgSnap2Event> data)
     {
-        //TODO
+        (Guid Id, bool Full)? previousCodeState = null; 
+        
+        foreach (ProgSnap2Event ev in data)
+        {
+            string codeStateDir;
+            if (ev.CodeStateId != null)
+            {
+                // If the codeStateId was set, a CodeState has been stored in the source for the 
+                // current event. We check if that respective directory actually exists and issue
+                // a warning if not.
+                codeStateDir = Path.Join(_codeStateExportDir, ev.CodeStateId.ToString());
+                if (!Directory.Exists(codeStateDir))
+                {
+                    // TODO Should maybe be ERROR; included markdown is currently showing in log file
+                    LogWarning($"CodeState not present in source directory\n" +
+                               $"\t[bold]SessionID[/]:\t{ev.SessionId}\n" +
+                               $"\t[bold]CodeStateID[/]:\t{ev.CodeStateId}");
+
+                    // TODO Determine what to do; copy the last complete one? 
+                    continue;
+                }
+                
+                // TODO Differentiate between full and partial states
+                previousCodeState = (ev.CodeStateId.Value, ev.FullCodeState.Value);
+            }
+
+            else
+            {
+                // If the codeStateId was not set, we know for sure that we will have to create a new directory
+                // in the export for the current event and register the corresponding ID in the exported database.
+                // Also, in all cases, we need to copy the contents of the previous codeState to this new directory.
+                
+                if (previousCodeState == null)
+                {
+                    // If no previous codeState is known, we are not able to perform any work for the current event,
+                    // thus we continue.
+                    // TODO Should a message be included here?
+                    continue;
+                }
+                ev.CodeStateId = new Guid();
+                codeStateDir = Path.Join(_codeStateExportDir, ev.CodeStateId.ToString());
+                Directory.CreateDirectory(codeStateDir);
+
+                string previousCodeStateDir = Path.Join(_codeStateExportDir, previousCodeState.Value.Id.ToString());
+                CopyDirectory(previousCodeStateDir, codeStateDir, true);
+                
+                // Now, some more work is required on a per-event basis.
+                switch (ev.EventType)
+                {
+                    case EventType.EvtFileDelete:
+                        // A file was deleted and thus we must delete it as well from the current CodeState.
+                        // In this case, the current codeState will also be the previous one for the next iteration.
+                        string path = Path.Join(codeStateDir, ev.CodeStateSection);
+                        File.Delete(path);
+                        previousCodeState = (ev.CodeStateId.Value, false);
+                        break;
+                    
+                    case EventType.EvtFileRename:
+                        // A file was renamed and thus we must rename it as well in the current CodeState.
+                        // In this case, the current codeState will also be the previous one for the next iteration.
+                        string oldPath = Path.Join(codeStateDir, ev.OldFileName);
+                        string newPath = Path.Join(codeStateDir, ev.CodeStateSection);
+                        File.Move(oldPath, newPath);
+                        previousCodeState = (ev.CodeStateId.Value, false);
+                        break;
+                }
+            }
+        }
     }
     #endregion
 
@@ -335,7 +406,7 @@ internal sealed class ProgSnapExportCommand : Command<ProgSnapExportCommand.Sett
     {
         bool toConsole = !_settings.Quiet;
         bool toFile = _settings.LogLevel >= LogLevel.Warning;
-        Log(message, "[orange]Warning:[/] ", toConsole, "[WARNING] ", toFile);
+        Log(message, "[yellow]Warning:[/] ", toConsole, "[WARNING] ", toFile);
     }
     
     /// <summary>
@@ -363,14 +434,59 @@ internal sealed class ProgSnapExportCommand : Command<ProgSnapExportCommand.Sett
     {
         if (toConsole)
         {
-            AnsiConsole.Write(new Markup($"{consoleHead}{message}.\n"));
+            AnsiConsole.Write(new Markup($"{consoleHead}{message}\n"));
         }
 
         if (toFile)
         {
             using FileStream fs = File.Open(_logFile, FileMode.Append);
-            using StreamWriter sw = new StreamWriter(fs);
-            sw.WriteLine($"{fileHead}{message}.");
+            using StreamWriter sw = new(fs);
+            sw.WriteLine($"{fileHead}{message}");
+        }
+    }
+    #endregion
+    
+    #region Utils
+    /// <summary>
+    /// Copies the contents of the source directory to the destination directory.
+    /// </summary>
+    /// <see href="https://learn.microsoft.com/en-us/dotnet/standard/io/how-to-copy-directories">Source (Microsoft)</see>
+    /// <param name="sourceDir"></param>
+    /// <param name="destinationDir"></param>
+    /// <param name="recursive"></param>
+    /// <exception cref="DirectoryNotFoundException"></exception>
+    private static void CopyDirectory(string sourceDir, string destinationDir, bool recursive)
+    {
+        // Get information about the source directory
+        DirectoryInfo dir = new(sourceDir);
+
+        // Check if the source directory exists
+        if (!dir.Exists)
+        {
+            throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+        }
+
+        // Cache directories before we start copying
+        DirectoryInfo[] dirs = dir.GetDirectories();
+
+        // Create the destination directory
+        Directory.CreateDirectory(destinationDir);
+
+        // Get the files in the source directory and copy to the destination directory
+        foreach (FileInfo file in dir.GetFiles())
+        {
+            string targetFilePath = Path.Combine(destinationDir, file.Name);
+            file.CopyTo(targetFilePath);
+        }
+
+        // If recursive and copying subdirectories, recursively call this method
+        if (recursive)
+        {
+            foreach (DirectoryInfo subDir in dirs)
+            {
+                string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+                CopyDirectory(subDir.FullName, newDestinationDir, true);
+            }
         }
     }
     #endregion
